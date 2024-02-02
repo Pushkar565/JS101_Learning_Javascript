857efa10c23ab47943fdca8644bba8a37e3e1f45
Pushkar565 <kpushkar56@gmail.com>
2024-02-02 19:37:38 +0530

    mathematical ops file updated

---
+++ MAthematical_operation.js
@@ -0,0 +1,115 @@
+// var a = 10 
+
+// var b = 10
+
+
+// console.log(a + b + a)
+
+
+// Declaration 
+// Initialization
+// Assignment
+
+// var num = 100 
+
+// var num ;  // declaration
+// num = 100 ; // initialization
+
+// num = 200  // assigning it.
+
+// num = 400
+
+
+// console.log(num)
+
+
+// var num1 = 500 ;
+
+// var Num1 = 1000;
+
+// // var $12name = 600;
+
+// console.log(num1)
+
+
+var num = 50.5 ;
+var firstname = "ritesh" ; 
+var lastname = 'ranjan' ;
+var anything = '#%123Aalkglsa'
+var sentence = "Quick brown fox"
+var anonymous = "45"
+
+
+// var typeof = "lorem ipsum"
+
+// console.log(anything)
+
+// console.log(sentence)
+
+// typeof => this keyword is gonna tell what datatype is inside a variable 
+
+// console.log(typeof sentence)
+
+
+// var variablename = value(data type)
+
+
+// create 5 variables and store and print them :
+
+// 1) name
+// 2) a password
+// 3) score
+// 4) age
+// 5) email
+
+// var a = 1 ;
+// var b = 2;
+// var c = 3;
+// var d = "string";
+
+// console.log(a , b , c ,d)
+
+// var _name = "ben 10"
+// var password = "ben@123"
+// var score = 10
+// var age = 12
+// var email= "ben10@gmail.com"
+
+// // console.log(_name , password,score,age , email)
+// console.log(_name)
+// console.log(password)
+// console.log(score)
+
+// int num = 10 ;
+
+// string firstnam = "ritesh";
+
+var num = 2;
+num = 20 ;
+
+num = "this is string"
+
+// js is loosely typed.
+
+
+// console.log(num)
+
+
+var a = 10;
+
+a = 20;
+
+a = "25";
+a = '30' ;
+a = "123*****a;kfda"
+a = 78
+
+console.log(typeof a)
+
+console.log(typeof 42)
+
+var a = 1
+var b = 2
+
+console.log(A,B)
+
